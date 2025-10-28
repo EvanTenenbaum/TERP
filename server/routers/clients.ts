937373d94132c633016f42bc26457ad5b32e5656
EvanTenenbaum <2452448+EvanTenenbaum@users.noreply.github.com>
2025-10-28 15:05:01 -0400

    Disable authentication for clients router

--- server/routers/clients.ts
+++ server/routers/clients.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { protectedProcedure, router } from "../_core/trpc";
+import { publicProcedure as protectedProcedure, router } from "../_core/trpc";
 import * as clientsDb from "../clientsDb";
 import * as transactionsDb from "../transactionsDb";
 
