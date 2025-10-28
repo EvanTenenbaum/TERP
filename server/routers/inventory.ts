7faf0f20e9955077c22d9d16434f494f7cae4520
EvanTenenbaum <2452448+EvanTenenbaum@users.noreply.github.com>
2025-10-28 15:08:50 -0400

    Disable authentication for all routers to show mock data

--- server/routers/inventory.ts
+++ server/routers/inventory.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { protectedProcedure, router } from "../_core/trpc";
+import { publicProcedure as protectedProcedure, router } from "../_core/trpc";
 import { handleError, AppError } from "../_core/errors";
 import * as inventoryDb from "../inventoryDb";
 import * as inventoryUtils from "../inventoryUtils";
