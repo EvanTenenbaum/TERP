d94ccc1fa2c6299598570e39de7369fd51cd9735
EvanTenenbaum <2452448+EvanTenenbaum@users.noreply.github.com>
2025-11-03 13:29:47 -0500

    fix: Frontend bugs - React Router and missing widget exports

--- client/src/components/dashboard/widgets-v3/index.ts
+++ client/src/components/dashboard/widgets-v3/index.ts
@@ -1,9 +1,10 @@
 // Export all v3 widgets
-export { SalesByClientWidget } from './SalesByClientWidget';
-export { CashFlowWidget } from './CashFlowWidget';
-export { TransactionSnapshotWidget } from './TransactionSnapshotWidget';
-export { InventorySnapshotWidget } from './InventorySnapshotWidget';
-export { TotalDebtWidget } from './TotalDebtWidget';
-export { SalesComparisonWidget } from './SalesComparisonWidget';
-export { ProfitabilityWidget } from './ProfitabilityWidget';
-export { MatchmakingOpportunitiesWidget } from './MatchmakingOpportunitiesWidget';
+// TODO: These widgets haven't been created yet - commenting out to fix build
+// export { SalesByClientWidget } from './SalesByClientWidget';
+// export { CashFlowWidget } from './CashFlowWidget';
+// export { TransactionSnapshotWidget } from './TransactionSnapshotWidget';
+// export { InventorySnapshotWidget } from './InventorySnapshotWidget';
+// export { TotalDebtWidget } from './TotalDebtWidget';
+// export { SalesComparisonWidget } from './SalesComparisonWidget';
+// export { ProfitabilityWidget } from './ProfitabilityWidget';
+// export { MatchmakingOpportunitiesWidget } from './MatchmakingOpportunitiesWidget';
